555d66f57412fb6e94a91f8a2bd841c1cd151656
Matt Allan <matthew.james.allan@gmail.com>
2016-03-16 17:18:26 -0400

    Add .alexrc support.

--- tasks/alex.js
+++ tasks/alex.js
@@ -11,28 +11,48 @@
 module.exports = function(grunt) {
 
   var alex = require('alex'),
-      reporter = require('vfile-reporter');
+      reporter = require('vfile-reporter'),
+      findUp = require('find-up'),
+      readPkgUp = require('read-pkg-up');
 
   grunt.registerMultiTask('alex', 'Catch insensitive, inconsiderate writing.', function() {
     var options = this.options({
       fail: false
     });
 
-    var result = [];
+    var done = this.async();
+    var files = this.files;
+    var runAlex = function (files, allow) {
+      var result = [];
+      files.forEach(function(f) {
 
-    this.files.forEach(function(f) {
+        f.src.filter(function(filepath) {
+          var content = grunt.file.read(filepath);
+          result.push(alex(content, allow));
+        });
 
-      f.src.filter(function(filepath) {
-        var content = grunt.file.read(filepath);
-        result.push(alex(content));
+        if (!options.fail) {
+          grunt.log.write(reporter(result));
+        } else {
+          grunt.fail.warn(reporter(result));
+        }
+        return done();
       });
+    };
 
-      if (!options.fail) {
-        grunt.log.write(reporter(result));
-        return;
+    findUp('.alexrc').then(function (alexRcPath) {
+      if (!alexRcPath) {
+        return readPkgUp().then(function (pkg) {
+
+          if (pkg && pkg.pkg && pkg.pkg.alex && pkg.pkg.alex.allow) {
+            return runAlex(files, pkg.pkg.alex.allow);
+          }
+          return runAlex(files, []);
+        });
       }
-
-      grunt.fail.warn(reporter(result));
+      var alexRcContents = JSON.parse(grunt.file.read(alexRcPath));
+      var allow = alexRcContents.allow ? alexRcContents.allow : [];
+      return runAlex(files, allow);
     });
   });
 
